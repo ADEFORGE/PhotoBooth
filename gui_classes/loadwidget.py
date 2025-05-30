20154d6fc85277b774655c1a529c8940b4cb5149
deforge <arthur.deforge@ecole.ensicaen.fr>
2025-05-30 15:27:59 +0200

    +centrer gif

--- gui_classes/loadwidget.py
+++ gui_classes/loadwidget.py
@@ -10,21 +10,22 @@ class LoadWidget(QWidget):
 
         self.load_animation_label = QLabel(alignment=Qt.AlignCenter)
         self.load_animation_label.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
-        self.load_animation_label.setScaledContents(True)
-        
+        # Ne pas utiliser setScaledContents(True) pour éviter la déformation
+
         # Taille relative à l'écran
         screen = QApplication.primaryScreen()
         size = screen.size()
         w = int(size.width() * 0.2)
         h = int(size.height() * 0.2)
-        self.load_animation_label.setFixedSize(w, h)
-        
+        self.load_animation_label.setMinimumSize(w, h)  # Minimum, pas fixe
+
         # Utiliser QMovie pour animer le GIF
         self.movie = QMovie("./gui_template/load.gif")
         self.load_animation_label.setMovie(self.movie)
         self.movie.start()
 
-        layout.addWidget(self.load_animation_label, 0, 0, 1, 2)
+        # Centrer le label dans le layout
+        layout.addWidget(self.load_animation_label, 0, 0, 1, 1, alignment=Qt.AlignCenter)
 
     def fct(self):
-        return
+        return 
